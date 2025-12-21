96736c8f0504e4336dfb81a9f22a71bd1a9010fe
fore4022 <gsmpjw@gmail.com>
2025-12-22 00:28:36 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+    int i, j, k;
+    scanf("%d",&k);
+    for (i = 1; i <= k; i++)
+    {
+        for(j=1;j<i;j++)
+        {
+            printf(" ");
+        }
+        for (j = k; j >=i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

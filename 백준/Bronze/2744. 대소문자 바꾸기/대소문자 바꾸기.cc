cece6f1b259c5ebdf1643eb68a9c36a78bd497c2
fore4022 <gsmpjw@gmail.com>
2026-01-23 23:07:11 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	string str;
+
+	cin >> str;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] >= 'a')
+		{
+			str[i] -= 32;
+		}
+		else
+		{
+			str[i] += 32;
+		}
+	}
+
+	cout << str;
+}

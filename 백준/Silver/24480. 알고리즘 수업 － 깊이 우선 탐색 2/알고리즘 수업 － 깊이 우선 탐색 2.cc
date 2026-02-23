22c3727575082ff03d8f9d1bec6984373e5c3a83
fore4022 <gsmpjw@gmail.com>
2026-02-23 14:33:39 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 368 ms, Memory: 26520 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<set>
+#include<functional>
+
+using namespace std;
+
+vector<multiset<int, greater<int>>> graph;
+vector<int> visited;
+int index = 1;
+
+void dfs(int node)
+{
+    visited[node] = index;
+    index++;
+
+    for (int next : graph[node])
+    {
+        if (visited[next] == 0)
+        {
+            dfs(next);
+        }
+    }
+}
+
+int main()
+{
+    int n, m, o, a, b;
+
+    cin >> n >> m >> o;
+
+    graph.resize(n + 1);
+    visited.resize(n + 1, 0);
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a >> b;
+
+        graph[a].insert(b);
+        graph[b].insert(a);
+    }
+
+    dfs(o);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << visited[i] << '\n';
+    }
+}

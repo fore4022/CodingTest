8fd6c918fca42e572f4acf540285899026910290
fore4022 <gsmpjw@gmail.com>
2026-02-26 00:59:24 +0900

    [Bronze IV] Title: Laptop Sticker, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21591. Laptop Sticker/Laptop Sticker.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+
+	cin >> a >> b >> c >> d;
+
+	if (a - c >= 2 && b - d >= 2)
+	{
+		cout << "1";
+	}
+	else
+	{
+		cout << "0";
+	}
+}

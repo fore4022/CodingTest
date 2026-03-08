713923198e06505583a74ba0dd2e04b1c7e7b713
fore4022 <gsmpjw@gmail.com>
2026-03-08 14:54:12 +0900

    [Silver V] Title: BABBA, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> vec;
+	int n;
+
+	vec.push_back(1);
+	vec.push_back(1);
+
+	cin >> n;
+
+	for (int i = 1; i <= n - 2; i++)
+	{
+		vec.push_back(vec[i - 1] + vec[i]);
+	}
+
+	cout << vec[n - 2] << " " << vec[n - 1];
+}

06588d54922973688db94c5e9f4993e1509e7065
fore4022 <gsmpjw@gmail.com>
2026-04-13 04:48:30 +0900

    [Silver II] Title: N과 M (10), Time: 480 ms, Memory: 3136 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> vec, nums, numCnt, result;
+vector<bool> visited;
+map<vector<int>, bool> mp;
+int limitDepth;
+
+void dfs(int node, int depth = 1)
+{
+    visited[node] = true;
+    numCnt[nums[node - 1]]++;
+
+    result.push_back(node);
+
+    if (depth == limitDepth)
+    {
+        if (!mp[numCnt])
+        {
+            for (int i : result)
+            {
+                cout << nums[i - 1] << " ";
+            }
+
+            cout << '\n';
+
+            mp[numCnt] = true;
+        }
+    }
+    else
+    {
+        for (int i = 1; i <= vec.size() - 1; i++)
+        {
+            if (!visited[vec[i]])
+            {
+                dfs(vec[i], depth + 1);
+            }
+        }
+    }
+
+    visited[node] = false;
+    numCnt[nums[node - 1]]--;
+
+    result.pop_back();
+}
+
+int main()
+{
+    set<int> s;
+    int n, m, o;
+
+    cin >> n >> m;
+
+    limitDepth = m;
+
+    vec.resize(n + 1, 0);
+    numCnt.resize(10001, 0);
+    visited.resize(n + 1, 0);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> o;
+
+        vec[i] = i;
+
+        s.insert(o);
+        nums.push_back(o);
+    }
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 1; i <= n; i++)
+    {
+        dfs(i);
+    }
+}

d6bfcb258b3e05d7ddf40cf432eee17c936a2efd
fore4022 <gsmpjw@gmail.com>
2026-04-12 14:13:07 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+vector<int> vec, nums, result;
+vector<bool> visited;
+int limitDepth;
+
+void dfs(int node, int depth = 1)
+{
+    if (depth > limitDepth)
+    {
+        return;
+    }
+
+    visited[node] = true;
+
+    result.push_back(node);
+
+    for (int i = 1; i <= vec.size() - 1; i++)
+    {
+        if (!visited[vec[i]])
+        {
+            dfs(vec[i], depth + 1);
+        }
+    }
+
+    if (depth == limitDepth)
+    {
+        for (int i : result)
+        {
+            cout << nums[i - 1] << " ";
+        }
+
+        cout << '\n';
+    }
+
+    visited[node] = false;
+
+    result.pop_back();
+}
+
+int main()
+{
+    set<int> s;
+    int n, m, o;
+
+    cin >> n >> m;
+
+    limitDepth = m;
+
+    vec.resize(n + 1, 0);
+    visited.resize(n + 1, 0);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> o;
+
+        s.insert(o);
+
+        vec[i] = i;
+    }
+
+    nums = vector<int>(s.begin(), s.end());
+
+    for (int i = 1; i <= n; i++)
+    {
+        dfs(i);
+    }
+}

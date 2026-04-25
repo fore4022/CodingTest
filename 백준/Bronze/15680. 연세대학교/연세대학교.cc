6daab6ade5c192595b9acc97e81c8238ab779569
fore4022 <gsmpjw@gmail.com>
2026-04-25 14:51:58 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int o;
+
+	cin >> o;
+
+	if (o == 0)
+	{
+		cout << "YONSEI";
+	}
+	else
+	{
+		cout << "Leading the Way to the Future";
+	}
+}
